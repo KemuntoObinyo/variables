1d89886730b048913cb1fe9222731591325391ad
Winfrida Kemunto <obinyowinfridaa@gmail.com>
2023-02-02 07:23:22 +0300

    Variable project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var fullname = "frida k"
+    var age = 20
+    var phonenumber = "0765423756"
+    var weightInKg = 65
+    var isKenyan: Boolean = true
+    var notKenya: Boolean = false
+
+    println(isKenyan)
+}
+
+
+
+
+
+
+
+
+
+
